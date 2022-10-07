7e6b9a980155270b19273b646c4e122fadd553ec
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-10-07 17:24:35 +0700

    added enemies behavior

--- app/actors/enemy.js
+++ app/actors/enemy.js
@@ -1,37 +1,79 @@
 const enemy ={
-    x : Number,
-    y : Number,
-    health: 500,
-    damage: 1,
+    speed: 0.5,
 
-    attack: function(player){
-        if((enemy.x + 32 === player.x || enemy.x -32 ===player.x) || (enemy.y - 32 === player.y || enemy.y - 32 === player.y)){
-            player.health = player.health- enemy.damage;
+    attack: function(enemy){
+        player.health-=enemy.damage;
+    },
+
+    death: function (enemy, index){
+        //remove enemy from array if he is dead
+        if(enemy.health<=0){
+            enemies.splice(index,1);
         }
     },
 
-    death: function(){
-        if(player.health<=0){
-            player.x = 100;
-            player.y = 100;
-            player.health = 10000;
+    //silly algorithm to move close to the player
+    movement: function (enemy){
+
+        const tileSize = 32;
+        //old position to check for collision
+        const oldEnemyX = enemy.x;
+        const oldEnemyY = enemy.y;
+
+        //if player is on the right and in vision zone
+        if(player.x-enemy.x>=tileSize && player.x-enemy.x<=tileSize*4 && Math.abs(player.y-enemy.y)<=tileSize*4){
+            enemy.x+=this.speed;
+            checkWall(enemy, oldEnemyX, oldEnemyY);
         }
-    },
+
+        //if player is on the left and in vision zone
+        if(enemy.x-player.x>=tileSize && enemy.x-player.x<=tileSize*4 && Math.abs(player.y-enemy.y)<=tileSize*4){
+            enemy.x-=this.speed;
+            checkWall(enemy, oldEnemyX, oldEnemyY);
+        }
+
+        //if player is below the enemy and in vision zone
+        if(player.y-enemy.y>=tileSize && player.y-enemy.y<=tileSize*4 && Math.abs(player.x-enemy.x)<=tileSize*4){
+            enemy.y+=this.speed;
+            checkWall(enemy, oldEnemyX, oldEnemyY);
+        }
+
+        //if player position is higher than enemy
+        if(enemy.y-player.y>=tileSize && enemy.y-player.y<=tileSize*4 && Math.abs(player.x-enemy.x)<=tileSize*4){
+            enemy.y-=this.speed;
+            checkWall(enemy, oldEnemyX, oldEnemyY);
+        }
+    }
 
 }
+
+ function enemiesActions(){
+    enemies.forEach(element=>{
+        //if player is close less than 10 pixels - attack player
+        if(Math.abs(element.x - player.x)<=32 && Math.abs(element.y - player.y)<=32){
+            enemy.attack(element);
+            console.log(player.health);
+
+            //check if player dead
+            player.death();
+        }
+        //move enemy
+        enemy.movement(element);
+    })
+ }
 
 
 const enemies = [
     {
-        x: 150, y: 350,
+        x: 150, y: 350, health: 100, damage: 1,
     },
     {
-        x: 800, y:520
+        x: 800, y:520, health: 100, damage: 1,
     },
     {
-        x: 300, y:100
+        x: 300, y:100, health: 100, damage: 1,
     },
     {
-        x: 850, y: 100
+        x: 850, y: 100, health: 100, damage: 1,
     },
 ]

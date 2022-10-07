8e0f32e5c5a045f17dc807ef7e964c8447f14ef2
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-10-07 17:23:07 +0700

    added collision check for player and enemies

--- app/components/collision.js
+++ app/components/collision.js
@@ -1,5 +1,6 @@
 function checkItemTaken(){
     potions.slice(0).forEach((element)=>{
+        //if player steps on potion give health and delete it from array
         if((player.x===element.x) && Math.abs(player.y-element.y)<=10){
             potion.heal();
             potions.splice(potions.indexOf(element),1);
@@ -7,10 +8,30 @@ function checkItemTaken(){
     });
 
     swords.slice(0).forEach((element)=>{
+        //if player steps on sword give buff and delete it from array
         if((player.x===element.x) && Math.abs(player.y-element.y)<=10){
             sword.buff();
             swords.splice(swords.indexOf(element),1);
         }
     });
 
+}
+
+function checkWall(actor, oldX, oldY){
+    const tileSize = 32;
+
+    //going through all map to check all tiles
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[i].length; j++) {
+            //if tile is a wall
+            if (map[i][j] === 0) {
+                //check if actor stepping on wall tile but give one pixel to go through corridors smoothly
+                if(Math.abs(actor.x-j*tileSize)<=tileSize-2 && Math.abs(actor.y-i*tileSize)<=tileSize-2){
+                    //if actor on wall placing old position
+                    actor.x = oldX;
+                    actor.y = oldY;
+                }
+            }
+        }
+    }
 }

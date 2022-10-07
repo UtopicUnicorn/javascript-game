f85c858df3d1d1386d54092179a69584877c08d8
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-10-07 20:08:32 +0700

    added status bar

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 const FPS = 120;
 document.addEventListener('DOMContentLoaded',domloaded,false);
 
+
 function domloaded() {
 
         setInterval(function (){
+            document.getElementById('health').innerHTML = player.health;
+            document.getElementById('damage').innerHTML = player.damage
+
             draw();
             checkDirection();
             checkItemTaken();

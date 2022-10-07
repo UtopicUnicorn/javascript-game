3b9078256049bf94211a3997faeb813f1abfbc41
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-10-07 17:26:47 +0700

    changed fields

--- app/actors/player.js
+++ app/actors/player.js
@@ -1,20 +1,24 @@
 const player ={
     x : 100,
     y : 100,
-    health: 10000,
+    health: 500,
     damage: 1,
+    speed: 1,
 
     attack: function(enemy){
-        if((enemy.x + 32 === player.x || enemy.x -32 ===player.x) || (enemy.y - 32 === player.y || enemy.y - 32 === player.y)){
-            enemy.health = enemy.health- player.damage;
+        //if enemy in one tile from player - attack
+        if(Math.abs(player.x-enemy.x)<=32 && Math.abs(player.y-enemy.y)<=32){
+            enemy.health-=player.damage;
         }
     },
 
     death: function(){
-        if(player.health<=0){
-            player.x = 100;
-            player.y = 100;
-            player.health = 10000;
+        //respawn if player dies
+        if(this.health<=0){
+            this.x = 100;
+            this.y = 100;
+            this.health = 500;
+            this.damage = 1;
         }
     },
 

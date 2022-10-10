f07670a1e2de5123158f0e711f9b889e84a7d159
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-10-10 15:13:33 +0700

    added check for gameovers

--- app/index.js
+++ app/index.js
@@ -1,16 +1,24 @@
 const FPS = 120;
 document.addEventListener('DOMContentLoaded',domloaded,false);
 
+const count = 10;
 
 function domloaded() {
-
-        setInterval(function (){
+        mapGenerator();
+        let game =  setInterval(function (){
             document.getElementById('health').innerHTML = player.health;
-            document.getElementById('damage').innerHTML = player.damage
-
+            document.getElementById('damage').innerHTML = player.damage;
             draw();
             checkDirection();
             checkItemTaken();
             enemiesActions();
+            if(player.death()){
+                clearInterval(game)
+                alert('You have died. Please reload page to play again')
+            }
+            if(enemies.length===0){
+                clearInterval(game)
+                alert('Congratulations, you killed all enemies. Please reload page to play again')
+            }
         },1000/FPS)
 }
